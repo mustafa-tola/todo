5d1d721a73dedd2e6ea65e6f7c79d930263049fc
mustafa-tola <46423331+mustafa-tola@users.noreply.github.com>
2022-07-02 22:57:46 -0700

    made website
    made website

---
+++ app.js
@@ -0,0 +1,37 @@
+var list = document.getElementById("todo_list")
+var _input = document.getElementById("todo_data")
+var btn = document.getElementById("toBeChangedButton")
+
+function addTodo() {
+    var _li = document.createElement("li")
+    var _editButton = document.createElement("button")
+    var _deleteButton = document.createElement("button")
+    var editText = document.createTextNode("Edit")
+    var deleteText = document.createTextNode("Delete")
+    var liText = document.createTextNode(_input.value)
+    _li.appendChild(liText)
+    _editButton.appendChild(editText)
+    _deleteButton.appendChild(deleteText)
+    _editButton.setAttribute("onclick","editButton(this)")
+    _deleteButton.setAttribute("onclick","deleteButton(this)")
+    _li.appendChild(_editButton)
+    _li.appendChild(_deleteButton)
+    list.appendChild(_li)
+    _input.value = ""
+}
+
+function editButton(currentElement) {
+    currentElement.parentNode.remove()
+    var ind = currentElement.parentNode.innerText.indexOf("Edit")
+    _input.value = currentElement.parentNode.innerText.slice(0,ind)
+    btn.innerText = "Edit Todo"
+}
+
+function deleteButton(currentElement) {
+    console.log(currentElement.parentNode)
+    currentElement.parentNode.remove();
+}
+
+function delAllTodos(currentElement){
+    list.innerHTML = ''
+}
